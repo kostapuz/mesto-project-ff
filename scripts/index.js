acbfd6520b7ef7c53fcd63aa5e42c981b93d1a26
Konstantin Tkachenko <kirkos1990@yandex.ru>
2024-06-22 17:15:18 +0300

    Commit after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -8,25 +8,28 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
  function createCard (data, deleteCard) {
-  const clonedCardTemplate = cardTemplate.cloneNode(true);
-  const placesItem = clonedCardTemplate.querySelector('.places__item');
-  const cardImage = clonedCardTemplate.querySelector('.card__image');
-  const cardDeleteButton = clonedCardTemplate.querySelector('.card__delete-button');
-  const cardTitle = clonedCardTemplate.querySelector('.card__title');
-  const cardLikeButton = clonedCardTemplate.querySelector('.card__like-button');
+  
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardDeleteButton = card.querySelector('.card__delete-button');
+  const cardTitle = card.querySelector('.card__title');
+  const cardLikeButton = card.querySelector('.card__like-button');
 
   cardTitle.textContent = data.name;
   cardImage.src = data.link;
   cardImage.alt = 'Карточка c пейзажами';
 
   cardDeleteButton.addEventListener('click', deleteCard);
-  return placesItem;
+  
+  return card;
  }
+
 
 // @todo: Функция удаления карточки
 function deleteCard (event) {
-  event.target.closest('.places__item').remove();
+  event.target.closest('.card').remove();
 };
+
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(card => placesList.append(createCard(card, deleteCard)));
